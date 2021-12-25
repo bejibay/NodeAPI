e0947018a09f131ee39b5c82e0aeede42c0a8c13
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-25 13:12:07 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,34 +1,25 @@
 
-a. Working with virtual memory with JSON 
-writing the code
+a. Using virtual memory to store data.
 
 index.js
-var express = require('express');
-var app = express();
-var students = require("./router.js");
-app.use("/students", "students");
-app.listen(3000, function(){
-console.log('server is
-listening at port 3000');
 
-router.js
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var router = express.Router();
 app.use(bodyParser.urlencoded({extended:true});
+app.use(bodyParser.json());
 
 var students =[ {id:201, name:"Yola", age:1996,sex:"male"},
 {id:202,name:"Dupe",age:1998,sex:"female"},
 {id:203,name:"Tolu",age:"2001,sex:"male"}];
 
 //Get router for all Students
-router.get('/',function(req, res){
+app.get('/',function(req, res){
 console.log(students);
 res.json(students);
 });
 //Get router for individual student
-router.get("/:id([0-9]{3,})",function(req,res){
+app.get("/:id([0-9]{3,})",function(req,res){
 var currStudent = students.filter(function(student){
 if(students.id = req.params.id){return true;}
 });
@@ -40,29 +31,34 @@ res.json({message:"Bad Request"});}
 
 //POST Router to create a student record
 
-router.post("/",function(req, res){
+app.post("/",function(req, res){
 if(!req.body.name||!req.body.age||!req.body.sex){
 res.status(404);
 res.json({message:" Bad Request");}
 else{
-var newId=studentd[students.length-1].id+1;
+var newId=students[students.length-1].id+1;
 students.push({id:newId,
 name:req.body.name,age:req.body.age,sex:req.body.sex});}
 res.json(students);
+
 });
 //PUT router to update individual student record
 router.put("/:id",function(req,res){
 var updateindex = students.map(function(student){
-return students.id;}.indexOf(req.params.id);
-if(updateindex!=<-1 && req.body.name && req.body.age && req.body.sex){students[updateindex]={name:req.body.name,age:req.body.age,sex:req.body.sex};};});
+return students.id;}).indexOf(req.params.id);
+if(updateIndex!=<-1 && req.body.name && req.body.age && req.body.sex){
+students[updateIndex]={name:req.body.name,age:req.body.age,sex:req.body.sex};};});
 res.json(students);});
 
 //DELETE Router to delete a student record
-router.delete('/', function(req, res){
+app.delete('/', function(req, res){
 var removeIndex = students.map(function(student)){
 return students.id}).indexOf(req.params.id);}
 students.splash(removeindex,1);
 res.json(students);
 });
 
+app.listen(3000, function(){
+console.log('server is
+listening at port 3000');
 
