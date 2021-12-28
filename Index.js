29ae85e97cfbfdf10aeefbb7ab6f9d4a6d480946
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-28 12:24:39 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,16 +1,18 @@
-a. Using virtual memory to store data.
+//a. Using virtual memory to store data.
 
-index.js
+//index.js
 
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use(bodyParser.urlencoded({extended:true});
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-var students =[ {id:201, name:"Yola", year:1996,sex:"male"},
-{id:202,name:"Dupe",year:1998,sex:"female"},
-{id:203,name:"Tolu",year:"2001,sex:"male"}];
+var students = [ 
+{id:201,name:"Yola",year:1996,gender:"female"},
+{id:202,name:"Dupe",year:1998,gender:"female"},
+{id:203,name:"Tolu",year:2001,gender:"male"}
+];
 
 //Get router for all Students
 app.get('/',function(req, res){
@@ -31,33 +33,31 @@ res.json({message:"Bad Request"});}
 //POST Router to create a student record
 
 app.post("/",function(req, res){
-if(!req.body.name||!req.body.age||!req.body.sex){
-res.status(404);
-res.json({message:" Bad Request");}
+if(!req.body.name||!req.body.age||!req.body.gender)
+{res.status(404);
+res.json({message:" Bad Request"});}
 else{
 var newId=students[students.length-1].id+1;
 students.push({id:newId,
-name:req.body.name,age:req.body.age,sex:req.body.sex});}
+name:req.body.name,age:req.body.age,gender:req.body.gender});}
 res.json(students);
-
 });
 //PUT router to update individual student record
-router.put("/:id",function(req,res){
+app.put("/:id",function(req,res){
 var updateindex = students.map(function(student){
 return students.id;}).indexOf(req.params.id);
-if(updateIndex!=<-1 && req.body.name && req.body.age && req.body.sex){
-students[updateIndex]={name:req.body.name,age:req.body.age,sex:req.body.sex};};});
-res.json(students);});
+if(updateIndex !== -1 && req.body.name && req.body.age && req.body.gender){
+students[updateIndex]={name:req.body.name,age:req.body.age,gender:req.body.gender};}
+res.json(students);
+});
 
 //DELETE Router to delete a student record
 app.delete('/', function(req, res){
-var removeIndex = students.map(function(student)){
-return students.id}).indexOf(req.params.id);}
+var removeIndex = students.map(function(student){
+return students.id}).indexOf(req.params.id);
 students.splash(removeindex,1);
 res.json(students);
 });
 
-app.listen(3000, function(){
-console.log('server is
-listening at port 3000');
+app.listen(3000);
 
