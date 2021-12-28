d6b40c51ba0ceed54e81e860c4f98288fd8b235a
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-28 13:42:21 +0100

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,12 +1,14 @@
-b Working with MYSQL database
 
-index1.js
+// Working with MYSQL Database
+//codes in index1.js
+
+//index1.js
 
 var express = require('express');
 var app = express();
 var mysql = require ('mysql');
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true});
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 var con = mysql.createConnection({
 host:"localhost",
@@ -14,10 +16,9 @@ user:"root",
 password:123456,
 database:mydb});
 con.connect(function (err){
-if(err) throw err;
-console.log('Connected 
-to Database');});
-}
+if (err) throw err;
+console.log('connected!');
+});
 
 //Get router for all Students
 app.get('/',function(req, res){
@@ -25,40 +26,45 @@ var sql = "SELECT* FROM students";
 con.query(sql,function(err,result){
 if(err) throw err;
 res.status(200);
-res.json(result))
-}
+res.json(result);
+});
 });
 //Get router for individual student
 app.get('/:id',function(req,res){
 var sql="SELECT* from students where id=?";
 var value=req.params.id;
-con.query(sql,[value],function(err,result)
-if(err) throw err;
-{result.status(200);
-res.json(result);});});
+con.query(sql,[value],function(err,result){
+if (err) throw err;
+result.status(200);
+res.json(result);
+});
+});
 
 
 //API to create a student record
 
 app.post('/',function(req, res){
-if(!req.body.name||!req.body.age||!req.body.sex){
+if(!req.body.name||!req.body.year||!req.body.gender){
 res.status(400);
-res.json({message:" Bad Request");}
+res.json({message:" Bad Request"});}
 else{
-con.query("INSERTinto students(name,sex,age) 
-Values(req.body.name,req.body.sex,req.body.age)",function(err,result)
-{if(err)throw err;
-result.status(200);
-console.log(result);});
+var sql = "INSERT INTO students(name,year,gender) VALUES ?";
+var val =[req.body.name,req.body.year,req.body.gender];
+con.query(sql,[val],function(err,result){
+if (err) throw err;
+res.status(200);
+res.json(result);});}
+});
 
 //API to update individual student record
 app.put('/:id',function(req,res){
-if(!req.body.name||!req.body.age||!req.body.sex){
+if(!req.body.name||!req.body.year||!req.body.gender){
 res.status(400);
-res.json({message:"Bad Request"});
+res.json({message:"Bad Request"});}
 else{
 var sql="update students where id=req.params.id";
-Con.query(sql,function(err,result){if(err)throw err;
+con.query(sql,function(err,result){
+if (err) throw err;
 console.log(result.affected + row +'updated');});
 }
 });
@@ -66,12 +72,11 @@ console.log(result.affected + row +'updated');});
 app.delete('/:id', function(req,res){
 var sql='DELETE from students where id=req.params.id';
 con.query(sql,function(err,result){if(err)throw err;
-console.log(result.affected row+'updated');});
+console.log(result.affected() + 'row updated');});
 });
 
 app.listen(3000, function(){
-console.log('server is
-listening at port 3000');
+console.log('server is listening at port 3000');});
 
 
 
