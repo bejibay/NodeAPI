9f35ba13c3f448bdf3aac268f8bd228b911f94bd
bejibay <58639255+bejibay@users.noreply.github.com>
2021-12-29 17:12:44 +0100

    Update index3.js

--- index3.js
+++ index3.js
@@ -1,40 +1,43 @@
+console.log("Hello, World!");
 
-c. Working with NoSql MongoDB
+// Working with NoSql MongoDB
 
-1. You can work with Mongo DB directly
-2.You can work with MongoDB through mongoose
+// 1. You can work with Mongo DB directly
+// 2.You can work with MongoDB through mongoose
 
-1.Working with MongoDB directly
+// 1.Working with MongoDB directly
 
-index3.js
+// index3.js
 
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var app = express();
-app.use(bodyParser.urlencoded({extended:true});
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
  
 // create connection to mongodb
 
-Var MongoClient= require('mongodb').MongoClient;
-var url=mongoDB://localhost/mydb;
+var MongoClient= require('mongodb').MongoClient;
+var url= "mongodb://localhost:27017/mydb";
 var con =MongoClient.connect(url,function(err,db){
-If(err )throw err;
-console.log('connected to database');});
-}
+if(err)throw err;
+console.log('connected to database');
+});
+
 
 //API to get all students
-app.get('/',function(req,res){db.collection('students').find({}).toarray(function(err,result){
+app.get('/',function(req,res){db.collection('students').find({}).toArray(function(err,result){
 if (err)throw err;
 res.status(200);
-red.json(result);
+res.json(result);
 })});
 
 //API to get a student record
 app.get('/:id',function(req,res){
 var query ={id:req.params.id};
-db.collections(students).find(query).toArray(function(err,result){if(err)throw err;
+db.collection(students).find(query).toArray(function(err,result){
+if(err)throw err;
 res.status(200);
 res.json(result);
 });});
@@ -42,35 +45,37 @@ res.json(result);
 //API to create student record
 
 app.post('/',function(req,res){
-{if(req.body.name&&req.body.sex&&req.body.age){
-var data={name:req.body.name,sex:req.body.sex,age:req.body.age};
-db.collections('students').insertOne(data,function(err,result){
+if(req.body.name&&req.body.year&&req.body.gender){
+var data={name:req.body.name,year:req.body.year,age:req.body.gender};
+db.collection('students').insertOne(data,function(err,result){
 if(err)throw err;
-console.log(result.inserted Count+' document inserted');
+console.log(result.insertedCoun() +' document inserted');
 res.status(200);
 res.json(result);
-});});
+});
 }});
 
 //API to update student record
 
-app.put('/:id',function(req, res){if(req.body.name&&req.body.sex&&req.body.age){var query = {_id:req.params.id};
-var newValues = {$set:{name:req.body.name,sex:req.body.sex,age:req.body.age}};
-db.collections(students).updateOne(query,newValues,function(err,result)
-{if(err) throw err;
-console.log('student record updated');});
-)}
-});
+app.put('/:id',function(req, res){
+if(req.body.name&&req.body.year&&req.body.gender){var query = {_id:req.params.id};
+var newValues = {$set:{name:req.body.name,year:req.body.year,gender:req.body.gender}};
+db.collection(students).updateOne(query,newValues,function(err,result){
+if(err) throw err;
+console.log('student record updated');
+)};
+}});
 
-API to delete Record
+// API to delete Record
 app.delete('/:id',function(req,res){
-var query = ={_id:req.params.id};
-db.collections('students').deleteOne(query,function(err,result){if(err)throw err:
+var query = {_id:req.params.id};
+db.collection('students').deleteOne(query,function(err,result){
+if(err)throw err:
 console.log('record deleted');
 });
 });
 
-2.Working with MongoDB through Mongose
+// 2.Working with MongoDB through Mongose
 
 //codes in index3.js as shown below
 
@@ -90,7 +95,7 @@ res.status(200);
 res.json(response);})});
 
 
-API to create student record
+API to create student recbbord
 
 app.post('/', function(req,res){var newStudent=new Student({name:req.body.name,sex:req.body.sex,age:req.body.age});
 newStudent.save(function(err,response){if(err)throw err;
